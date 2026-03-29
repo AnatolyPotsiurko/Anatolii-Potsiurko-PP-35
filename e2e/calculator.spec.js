a8b0e4eeb6bfe07a36f1f2e43b25c787d589602b
AnatolyPotsiurko <143560033+AnatolyPotsiurko@users.noreply.github.com>
2026-03-29 04:45:08 +0300

    Додав налаштування CI/CD пайплайну

--- e2e/calculator.spec.js
+++ e2e/calculator.spec.js
@@ -1,14 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('calculator adds numbers', async ({ page }) => {
-
   await page.goto('http://localhost:3000');
 
+  // Використання document в браузерному контексті через evaluate
+  await page.evaluate((viteStatus) => {
+    const statusElement = document.createElement('div');
+    statusElement.textContent = viteStatus;  // Використовуємо передану змінну
+    statusElement.style.position = 'fixed';
+    document.body.appendChild(statusElement);
+  }, process.env.VITE_APP_STATUS);  // Передаємо значення змінної
+
+  // Ваші кроки тесту
   await page.click('[data-digit="2"]');
-  await page.click('[data-op="+"]');
+  await page.click('[data-action="+"]');
   await page.click('[data-digit="3"]');
   await page.click('[data-action="equals"]');
 
   await expect(page.locator('#display')).toHaveValue('5');
-
 });
